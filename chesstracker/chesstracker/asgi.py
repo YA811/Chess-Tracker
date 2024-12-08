23390ce5cced03a1362c71bf775f74ac1b0ee624
YA811 <yousifahmedd21@gmail.com>
2024-12-08 11:05:00 +0300

    starting up

--- chesstracker/chesstracker/asgi.py
+++ chesstracker/chesstracker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Chess_Tracker project.
+ASGI config for chesstracker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chess_Tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chesstracker.settings')
 
 application = get_asgi_application()
