5f90a6caeb83792ef648c95005e5556301d5b662
YA811 <yousifahmedd21@gmail.com>
2024-12-08 15:08:22 +0300

    adding the training sessions

--- chesstracker/main_app/urls.py
+++ chesstracker/main_app/urls.py
@@ -4,5 +4,6 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-      path('game/', views.add_game, name='add_game'),
+    path('game/', views.add_game, name='add_game'),
+    path('training-session/', views.add_training_session, name='add_training_session'),
 ]
