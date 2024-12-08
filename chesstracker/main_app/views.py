6fb092db229d5961bdff783375585cd069afd35f
YA811 <yousifahmedd21@gmail.com>
2024-12-08 13:49:51 +0300

    done with the forms

--- chesstracker/main_app/views.py
+++ chesstracker/main_app/views.py
@@ -1,9 +1,11 @@
 # main_app/views.py
 
-from django.shortcuts import render
-
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Game, TrainingSession
+from .forms import GameForm, TrainingSessionForm
+
 
 # Define the home view function
 def home(request):
@@ -12,3 +14,29 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def add_game(request):
+    if request.method == 'POST':
+        form = GameForm(request.POST)
+        if form.is_valid():
+            game = form.save(commit=False)
+            game.user = request.user
+            game.save()
+            return redirect('home')
+    else:
+        form = GameForm()
+    return render(request, 'add_game.html', {'form': form})
+
+
+def add_training_session(request):
+    if request.method == 'POST':
+        form = TrainingSessionForm(request.POST)
+        if form.is_valid():
+            session = form.save(commit=False)
+            session.user = request.user
+            session.save()
+            return redirect('home')
+    else:
+        form = TrainingSessionForm()
+    return render(request, 'add_training_session.html', {'form': form})
+
