8f1349f33470e302bdf9cf9277d8e670e1e5ef34
YA811 <yousifahmedd21@gmail.com>
2024-12-10 10:41:52 +0300

    fixing

--- chesstracker/main_app/urls.py
+++ chesstracker/main_app/urls.py
@@ -4,8 +4,8 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('game/', views.add_game, name='add_game'),
+    path('game/new/', views.add_game, name='add_game'),
     path('your-games/', views.game_list, name='your_games'),
-    path('training-session/', views.add_training_session, name='add_training_session'),
+    path('training-session/new/', views.add_training_session, name='add_training_session'),
     path('training-sessions/', views.training_sessions, name='training_sessions'), 
 ]
