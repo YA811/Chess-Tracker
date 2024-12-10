24c0d271ab41d86650fa66acc3e15f3691c525a6
YA811 <yousifahmedd21@gmail.com>
2024-12-10 13:56:35 +0300

    adding the details

--- chesstracker/main_app/urls.py
+++ chesstracker/main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('your-games/', views.game_list, name='your_games'),
     path('training-session/new/', views.add_training_session, name='add_training_session'),
     path('training-sessions/', views.training_sessions, name='training_sessions'), 
+    path('game/<int:game_id>/', views.game_details, name='game_details'),
+    path('training_session/<int:session_id>/', views.training_session_detail, name='training_session_detail'),
 ]
