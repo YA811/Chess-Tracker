e3f9b2d64cef513026a48d33f768b50380ce073d
YA811 <yousifahmedd21@gmail.com>
2024-12-12 09:53:38 +0300

    last editing

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,6 @@ from .forms import GameForm, TrainingSessionForm
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
 from django.views.generic import ListView, DetailView
-# Add the two imports below
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
@@ -104,24 +103,14 @@ def update_game(request, game_id):
 def signup(request):
     error_message = ''
     if request.method == 'POST':
-        # This is how to create a 'user' form object
-        # that includes the data from the browser
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            # This will add the user to the database
             user = form.save()
-            # This is how we log a user in
             login(request, user)
             return redirect('about')
         else:
             error_message = 'Invalid sign up - try again'
-    # A bad POST or a GET request, so render signup.html with an empty form
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'signup.html', context)
-    # Same as: 
-    # return render(
-    #     request, 
-    #     'signup.html',
-    #     {'form': form, 'error_message': error_message}
-    # )
+ 
